4c3a8a27cc454ff1eef02de479b6b87596692dfe
ifypec4life <99217499+ifypec4life@users.noreply.github.com>
2022-10-21 19:04:19 -0400

    second commit

--- JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
+++ JavaWebApp/src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,6 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-string name = "ifeoma";
+string name = "peculiar";
 }
 system.out.print(name);
